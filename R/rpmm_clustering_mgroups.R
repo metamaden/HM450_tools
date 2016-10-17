5293aa0415ea6a9efd49089a1fd0840c1499cd7d
Sean <metamaden@users.noreply.github.com>
2016-10-16 23:23:03 -0700

    Generalize for arbitrarily many clusters

--- R/rpmm_clustering_mgroups.R
+++ R/rpmm_clustering_mgroups.R
@@ -9,10 +9,17 @@ names(a) <- levels(rpmmClass)
 m <- matrix(as.character(unlist(a)))
 
 # clu matrix dimensions contingent on cluster count (ie. 4x clusters shown below)
-clu <- matrix(c(rep(names(a)[[1]],
-                    length(a[[1]])),rep(names(a)[[2]],length(a[[2]])),
-                rep(names(a)[[3]], length(a[[3]])),
-                rep(names(a)[[4]], length(a[[4]]))))
+x <- levels(as.factor(a))
+eval.clu <- "matrix(c("
+for(i in 1:(length(x)-1)){eval.clu <- paste(eval.clu,paste("rep(names(a)[[",i,"]], length(a[[",i,"]]))",sep=""),collapse=",")}
+eval.clu <- paste(eval.clu,paste("rep(names(a)[[",length(x),"]], length(a[[",length(x),"]]))))",sep=""),collapse=",")
+eval(parse(text=paste0(eval.clu)))
+
+# example clu:
+#clu <- matrix(c(rep(names(a)[[1]],
+#                length(a[[1]])),rep(names(a)[[2]],length(a[[2]])),
+#                rep(names(a)[[3]], length(a[[3]])),
+#                rep(names(a)[[4]], length(a[[4]]))))
 
 # construct dataframe of samples and clusters
 asg <- data.frame(cbind(m, clu))
