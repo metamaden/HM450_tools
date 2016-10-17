f66d311275edb13adc20d2f045e2e211f9713a07
Sean <metamaden@users.noreply.github.com>
2016-10-17 12:55:15 -0700

    Update genecorr_methylationHM450_expression.R

--- R/Island_Drift/genecorr_methylationHM450_expression.R
+++ R/Island_Drift/genecorr_methylationHM450_expression.R
@@ -49,6 +49,54 @@ GENE_methyexpr_corr = function(gene="SOX15", dat.expr=EXPR, dat.mex=gse.krause,
           y = dat.expr[g,ids.expr]
         }
   
+GENE_methyexpr_corr = function(gene="SOX15", dat.expr=EXPR, dat.mex=gse.krause,
+                               ids.mex=mex.array.ids, ids.expr=arrayGSE.EAC, 
+                               log2FC=TRUE, ctfilter=NULL, lgndcex=1) {
+  eval(parse(text=paste0("par(mfrow=c(",length(gene),",",2,"))")))
+  for(k in 1:length(gene)){
+  
+    g <- gene[k]
+    message("Current Gene ID: '",g,"'")
+    str1 = paste0("(^|;)",g,"(;|$)")
+    
+    if(!g %in% rownames(dat.expr)|nrow(manifestData[grepl(str1,manifestData$UCSC_RefGene_Name),])==0){
+      message("ERROR: Gene ID '",g,"' not in both expression and methylation data!\nSkipping...\n###")
+    } else{
+      message("Analyzing Gene ID '",g,"'...")
+    
+      Isl = unique(manifestData[grepl(str1,manifestData$UCSC_RefGene_Name),"Islands_Name"])
+      
+      if(length(intersect(Isl,ILS.hypo.drift))==0){
+        message("No drift islands in provided list overlap gene '",g,"',\nSkipping...\n####")
+      } else{
+      
+        Isl = intersect(Isl,ILS.hypo.drift) # allows only islands that have a least one drift CpG
+  
+        cpgs.GENE = manifestData[grepl(str1,manifestData$UCSC_RefGene_Name),"Name"]
+        isl.GENE = manifestData[cpgs.GENE,"Islands_Name"]
+    
+        # get beta values and island/gene info for HM450 array
+        dat.GENE = getBeta(dat.mex[cpgs.GENE,ids.mex])
+        out = CpGisl.level.info(set = Isl, isls = isl.GENE, cpgs = cpgs.GENE, dat=dat.GENE) 
+        nr =  nrow(out$Mvals)
+        tmp = message("All Genes Overlapping Methylation Probes at Islands Analyzed...\n", out$genes)
+    
+      
+        if(!is.null(nr)) {
+         x = (out$Mvals[1,])
+         message('number of unique islands for gene ',g,': ',nr,'\n')
+        } else {
+         x = (out$Mvals)
+         message('Number of unique islands for gene ',g,': ',1,'\n')
+        }
+    
+        # convert from log2FC to counts
+        if(log2FC==TRUE){
+          y = 2^dat.expr[g,ids.expr]  
+        } else{
+          y = dat.expr[g,ids.expr]
+        }
+  
         # apply count filter before correlation analysis
         if(!is.null(ctfilter)){
           x <- x[which(y>=ctfilter)]
@@ -68,10 +116,10 @@ GENE_methyexpr_corr = function(gene="SOX15", dat.expr=EXPR, dat.mex=gse.krause,
         lines(loess.smooth(x,y,span=.8),lwd=2,lty=3)
         tmp = cor.test(as.numeric(x),as.numeric(y))
         corr[1] = tmp$estimate; pval[1] = tmp$p.value
-        message(out$islands[1],'\nCorrelation Results: Pearson R = ',round(corr[1],3),', p-val: ',round(pval[1],3),'\n')
+        message(out$islands[1],'\nCorrelation Results: Pearson R = ',round(corr[1],3),', p-val: ',round(pval[1],6),'\n')
       
         # keep cortest results
-        legendtxt <- paste0(Isl[1],", R = ",round(corr[1],3),", p-val = ",round(pval[1],3),"\n")
+        legendtxt <- paste0(Isl[1],", R = ",round(corr[1],3),", p-val = ",round(pval[1],6),"\n")
   
         if(!is.null(nr)){
           for(i in 2:nr) {
@@ -86,8 +134,8 @@ GENE_methyexpr_corr = function(gene="SOX15", dat.expr=EXPR, dat.mex=gse.krause,
           lines(loess.smooth(x,y,span=.8),lwd=2,lty=3,col=i)
           tmp = cor.test(as.numeric(x),as.numeric(y))
           corr[i] = tmp$estimate; pval[i] = tmp$p.value
-          message(out$islands[i],'\nCorrelation Results: Pearson R = ',round(corr[i],3),', p-val: ',round(pval[i],3),'\n####')
-          legendtxt <- c(legendtxt,paste0(Isl[i],", R = ",round(corr[i],3),", p-val = ",round(pval[i],3),"\n"))
+          message(out$islands[i],'\nCorrelation Results: Pearson R = ',round(corr[i],3),', p-val: ',round(pval[i],6))
+          legendtxt <- c(legendtxt,paste0(Isl[i],", R = ",round(corr[i],3),", p-val = ",round(pval[i],6),4),"\n")
           }
         }
         message("####")
